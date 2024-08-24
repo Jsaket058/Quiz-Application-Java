0c0fa5e36a150f5eb0007a5e60c01bdd16b93abf
Saket Jain <jsaket058@gmail.com>
2024-08-24 12:05:34 +0530

    final

--- Quiz1.java
+++ Quiz1.java
@@ -13,16 +13,20 @@ class FDemo2 extends JFrame {
     }
 }
 
-class Demo2 extends JPanel {
+class Demo2 extends JPanel implements ActionListener {
     ImageIcon img2;
     Image quiz;
     JLabel qno, question;
     String[][] questions = new String[10][5];
     String[][] answers = new String[10][2];
+    String[][] useranswers = new String[10][1];
     JRadioButton opt1, opt2, opt3, opt4;
     ButtonGroup groupoptions;
     JButton next, lifeline, submit;
     public static int timer = 15;
+    public static int ans_given = 0;
+    public static int count = 0;
+    public static int score = 0;
 
     Demo2() {
         img2 = new ImageIcon("quiz.jpg");
@@ -134,6 +138,7 @@ class Demo2 extends JPanel {
         next.setFont(new Font("Tahoma", Font.PLAIN, 22));
         next.setBackground(new Color(30, 144, 255));
         next.setForeground(Color.WHITE);
+        next.addActionListener(this);
         add(next);
 
         lifeline = new JButton("50-50 Lifeline");
@@ -141,6 +146,7 @@ class Demo2 extends JPanel {
         lifeline.setFont(new Font("Tahoma", Font.PLAIN, 22));
         lifeline.setBackground(new Color(30, 144, 255));
         lifeline.setForeground(Color.WHITE);
+        lifeline.addActionListener(this);
         add(lifeline);
 
         submit = new JButton("Submit");
@@ -149,8 +155,9 @@ class Demo2 extends JPanel {
         submit.setBackground(new Color(30, 144, 255));
         submit.setForeground(Color.WHITE);
         submit.setEnabled(false);
+        submit.addActionListener(this);
         add(submit);
-        start(0);
+        start(count);
         setLayout(null);
     }
 
@@ -158,9 +165,18 @@ class Demo2 extends JPanel {
         qno.setText("" + (count + 1) + ". ");
         question.setText(questions[count][0]);
         opt1.setText(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]);
+
         opt2.setText(questions[count][2]);
+        opt2.setActionCommand(questions[count][2]);
+
         opt3.setText(questions[count][3]);
+        opt3.setActionCommand(questions[count][3]);
+
         opt4.setText(questions[count][4]);
+        opt4.setActionCommand(questions[count][4]);
+
+        groupoptions.clearSelection();
     }
 
     public void paintComponent(Graphics g) {
@@ -171,8 +187,7 @@ class Demo2 extends JPanel {
         g.setFont(new Font("Tahoma", Font.BOLD, 15));
         if (timer > 0) {
             g.drawString(time, 1020, 300);
-        }
-        else{
+        } else {
             g.drawString("Times up!!", 1020, 300);
         }
         timer--;
@@ -182,9 +197,89 @@ class Demo2 extends JPanel {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        if (ans_given == 1) {
+            ans_given = 0;
+            timer = 15;
+        } else if (timer < 0) {
+            timer = 15;
+
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            if (count == 9) { // submit button
+                if (groupoptions.getSelection() == null) {
+                    useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+
+                for (int i = 0; i < useranswers.length; i++) {
+                    if (useranswers[i][0].equals(answers[i][1]))
+                        score += 10;
+
+                }
+                setVisible(false);
+            } else { // next button
+                if (groupoptions.getSelection() == null) {
+                    useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                count++;
+                start(count);
+            }
+        }
     }
 
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == next) {
+            repaint();
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+                useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
 
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            start(++count);
+        } else if (ae.getSource() == lifeline) {
+            if (count == 2 || count == 4 || count == 6 || count == 8 || count == 9) {
+                opt2.setEnabled(false);
+                opt3.setEnabled(false);
+            } else {
+                opt1.setEnabled(false);
+                opt4.setEnabled(false);
+            }
+            lifeline.setEnabled(false);
+        } else {
+            ans_given=1;
+            if (groupoptions.getSelection() == null) {
+                useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+
+            for (int i = 0; i < useranswers.length; i++) {
+                if (useranswers[i][0].equals(answers[i][1]))
+                    score += 10;
+            }
+            setVisible(false);
+        }
+    }
 }
 
 class Quiz1 {
--- Rules.java
+++ Rules.java
@@ -1,91 +1,70 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
 
-class FDemo1 extends JFrame {
-    Demo1 dm;
+public class Rules extends JFrame implements ActionListener{
 
-    FDemo1(String name) {
-        super.setTitle("Rules");
-        dm = new Demo1(name);
-        add(dm);
-    }
-}
-
-class Demo1 extends JPanel implements ActionListener {
-    JLabel heading11;
-    JLabel rules;
-
-    JButton b11, b21;
-
-    Demo1(String name) {
-        heading11 = new JLabel("Welcome " + name + " to Simple Minds");
-        heading11.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
-        heading11.setBounds(50, 20, 700, 40);
-        heading11.setForeground(new Color(30, 144, 254));
-        add(heading11);
-
-        rules = new JLabel();
-        rules.setBounds(20, 95, 730, 320);
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
         rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
         rules.setText(
-                "<html>" +
-                        "1. We are trained to be a programmer and not a story teller, answer point to point"
-                        + "<br><br>" +
-                        "2. Do not unnecessarily smile at the person sitting next to you, they may also not know the answer"
-                        + "<br><br>" +
-                        "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>"
-                        +
-                        "4. Crying is allowed but please do so quietly." + "<br><br>" +
-                        "5. Only a fool asks and a wise answers (Be wise, not otherwise)" + "<br><br>" +
-                        "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di"
-                        + "<br><br>" +
-                        "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
-                        "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
-                        "<html>"
-
+            "<html>"+ 
+                "1. You are trained to be a programmer and not a story teller, answer point to point" + "<br><br>" +
+                "2. Do not unnecessarily smile at the person sitting next to you, they may also not know the answer" + "<br><br>" +
+                "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>" +
+                "4. Crying is allowed but please do so quietly." + "<br><br>" +
+                "5. Only a fool asks and a wise answers (Be wise, not otherwise)" + "<br><br>" +
+                "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di" + "<br><br>" +
+                "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
+                "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
+            "<html>"
         );
         add(rules);
-
-        b11 = new JButton("Back");
-        b11.setBounds(520, 400, 120, 30);
-        b11.setBackground(new Color(30, 144, 254));
-        b11.setForeground(Color.WHITE);
-        b11.addActionListener(this);
-        add(b11);
-
-        b21 = new JButton("Start");
-        b21.setBounds(20, 400, 120, 30);
-        b21.setBackground(new Color(30, 144, 254));
-        b21.setForeground(Color.WHITE);
-        b21.addActionListener(this);
-        add(b21);
-        setLayout(null);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
     }
-
+    
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == b11) {
+        if (ae.getSource() == start) {
             setVisible(false);
-            FDemo f = new FDemo();
-            f.setVisible(true);
-            f.setLocation(200, 150);
-            f.setSize(1200, 500);
-            f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
-        }
-
-        else if (ae.getSource() == b21) {
-
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
         }
     }
-}
-
-class Rules {
+    
     public static void main(String[] args) {
-        FDemo1 f1 = new FDemo1("name");
-        f1.setSize(1200, 500);
-        f1.setLocation(200, 150);
-        f1.setVisible(true);
-        f1.setLayout(null);
-        f1.setDefaultCloseOperation(f1.EXIT_ON_CLOSE);
+        new Rules("User");
     }
 }
